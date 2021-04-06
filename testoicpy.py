9c2f302fa97bc22e874b7d434b7a48617aa86cf0
wenjian80 <33510286+wenjian80@users.noreply.github.com>
2021-04-06 16:49:39 +0800

    commit

--- testoicpy.py
+++ testoicpy.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Created on Mon Apr  5 14:22:09 2021
+
 @author: bjlim
 """
 
@@ -8,13 +10,14 @@ import time
 import datetime
 import requests
 import csv
+#import cx_Oracle
 
-#Set headers, witht he authroization. us epostman to get the Authorization basic encryption
-my_headers  = {"Content-Type": "application/json", "Authorization": "Basic keyinyoutown", "X-HTTP-Method-Override": "PATCH"}
+#Set headers, witht he authroization. us epostman to get the Authorization
+my_headers  = {"Content-Type": "application/json", "Authorization": "Basic changeit", "X-HTTP-Method-Override": "PATCH"}
 
 
 #Filter by 6h based on which integration you want to filter
-response = requests.get("https://youroicurl/ic/api/integration/v1/monitoring/instances?q={timewindow:'6h', code:'yourintegrationname'}", headers=my_headers)
+response = requests.get("https://changeit/ic/api/integration/v1/monitoring/instances?q={timewindow:'6h', code:'TESTTHAIOIC'}", headers=my_headers)
 
 responseJson = response.json()
 
@@ -35,25 +38,55 @@ printArray = []
 #write csv header
 with open('timetaken.csv','a') as fd:            
     fd.write("Instance Id|Total Time taken(millseconds)|Timestamp|Message|Time  Taken" + "\n")
-	
+
+#Open connection to json db in oracle cloud
+#Refer to https://www.oracle.com/database/technologies/appdev/python/quickstartpython.html#copy
+#CREATE TABLE "ADMIN"."ACTIVITY_STREAM" 
+#   (	"INSTANCE_ID" NUMBER(*,0), 
+#	"TOTAL_TIME_TAKEN" NUMBER(*,0), 
+#	"TIMESTAMP" VARCHAR2(200 BYTE) COLLATE "USING_NLS_COMP", 
+#	"MESSAGE" CLOB COLLATE "USING_NLS_COMP", 
+#	"TIME_TAKEN" NUMBER(*,0)
+#   )
+#CREATE TABLE "ADMIN"."INSTANCE_PAYLOAD" 
+#   (	"INSTANCE_ID" NUMBER(*,0), 
+#	"PAYLOAD" CLOB COLLATE "USING_NLS_COMP"
+#   )
+#alter table INSTANCE_PAYLOAD add constraint "ENSURE_JSON" check (PAYLOAD is json) enable
+#cx_Oracle.init_oracle_client(lib_dir=r"D:\instantclient_19_10")
+#connection = cx_Oracle.connect(user="ADMIN", password="changeit", dsn="changeit")
+#cursor = connection.cursor()
+
+#insertInstance = ('insert into INSTANCE_PAYLOAD '
+#        'values(:vinstanceId,:vPayload)')
+
+#insertSql = ('insert into ACTIVITY_STREAM '
+#        'values(:vinstanceId,:vTotalTimeTaken,:vTimestamp,:vMessage, :vTimetaken)')
+
+#updateSql = ('update ACTIVITY_STREAM set TOTAL_TIME_TAKEN=:vTotalTimeTaken where INSTANCE_ID=:vinstanceId')
+
+                
 for instance in instanceArray:    
-    url = "https://youroicurl/ic/api/integration/v1/monitoring/instances/" + instance + "/activityStream"
+    url = "https://changeit/ic/api/integration/v1/monitoring/instances/" + instance + "/activityStream"
     response = requests.get(url, headers=my_headers)        
     data = response.json()
     mylists = data['ascList']
     
+#    cursor.execute(insertInstance, [instance, str(data)])
     
     for list in mylists:        
         date_time_str = list['timestamp']    
         
         if (i ==0) :    
             printArray.append(list['timestamp'] + "|" + list['message'].strip('\n').replace('\n', '') + "|0")
+#            cursor.execute(insertSql, [instance, 0, list['timestamp'], list['message'].strip('\n').replace('\n', ''), 0])
         else :        
             diff = datetime.datetime.strptime(date_time_str, datetimeFormat)\
                    - datetime.datetime.strptime(previous_date_time_str, datetimeFormat)
             diffInMillSeconds = diff.microseconds/1000
             totalTimeTaken = diffInMillSeconds + totalTimeTaken
             printArray.append(list['timestamp'] + "|" + list['message'].strip('\n').replace('\n', '') + "|" + str(diffInMillSeconds))
+#            cursor.execute(insertSql, [instance, 0, list['timestamp'], list['message'].strip('\n').replace('\n', ''), str(diffInMillSeconds)])
            
         i = i + 1
         previous_date_time_str = date_time_str
@@ -62,7 +95,11 @@ for instance in instanceArray:
     for row in printArray:            
         with open('timetaken.csv','a') as fd:            
             fd.write(instance + "|" + str(totalTimeTaken) + "|" + row + "\n")            
-
+    
+    
+#    cursor.execute(updateSql,[instance,str(totalTimeTaken)])
     totalTimeTaken=0
     i = 0
     printArray = []
+
+#connection.commit()
